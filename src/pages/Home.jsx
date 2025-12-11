d40c0e3f7b03e43d5fe120dc8f302a7e9e14f8bf
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-12-11 17:15:46 +0100

    add link index from express

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,19 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+
+    const [films, setFilms] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/movies')
+            .then(response => {
+                console.log(response)
+            }).catch(err => {
+                console.log(err.message)
+            })
+    }, [])
+
     return (
         <>
             <h1>Wlecome to or Films reviews app</h1>
